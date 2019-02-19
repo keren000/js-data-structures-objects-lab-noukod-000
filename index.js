b717daee6c1dd01d0967996da12cfb97d5601fc2
Keren-Happuc Fleurimond <keren@noukod.com>
2019-02-19 14:56:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function updateDriverWithKeyAndValue(driver, key, value)
  {
    return Object.assign({}, driver, { [key]: value });
  }
+
+ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) 
+ {
+   driver[key] = value;
+   return driver;
+ }
+ 
